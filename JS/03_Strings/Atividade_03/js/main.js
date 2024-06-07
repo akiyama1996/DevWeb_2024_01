2ede01d93e29bd0504dc818f6bfaefc949b43d1c
akiyama1996 <m.akiyama1996@outlook.com>
2024-06-06 21:34:22 -0300

    subida atv 3

---
+++ JS/03_Strings/Atividade_03/js/main.js
@@ -0,0 +1,16 @@
+var frutas = ["Banana","Maça","Laranja","Manga",""]
+
+console.log (frutas.length);
+console.log (frutas[0]);
+console.log (frutas[1]);
+console.log (frutas[2]);
+console.log (frutas[3]);
+console.log (frutas[4]);
+let text = ' Frutas Saudáveis! ';
+console.log(text.trim());
+console.clear();
+const escolha = 'Escolha sua fruta!';
+console.log(escolha.toUpperCase());
+console.log(escolha.toLowerCase());
+
+
